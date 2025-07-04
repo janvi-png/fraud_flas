c488bf5552e42d4bc2fd3ce6361d6f9699656dfb
janvi-png <sharmajjjanvi@gmail.com>
2025-07-04 16:23:17 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,19 +1,19 @@
-#1. import libraries
+
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 import pickle
 
-# Load dataset
-df = pd.read_csv('C:/Users/janvi/OneDrive/Desktop/DMA_LAB/fraud_flas/Bank_Transaction_Fraud_Detection.csv')  
+csv_url = "https://drive.google.com/uc?export=download&id=12dX71hdB6qNeuK3CJ3VxPJtLLMckKsFJ"
+df = pd.read_csv(csv_url)
 df = df.dropna()
 
-# Select  features i.e X & Y
-selected_features = ['Transaction_Amount', 'Age', 'Account_Balance', 'Account_Type', 'City','Gender']
+
+selected_features = ['Transaction_Amount', 'Age', 'Account_Balance', 'Account_Type', 'City', 'Gender']
 target_col = 'Is_Fraud'
 
-# Label ENCODING 
+
 encoders = {}
 for col in selected_features:
     if df[col].dtype == 'object':
@@ -25,24 +25,22 @@ X = df[selected_features]
 y = df[target_col]
 
 
-# Save the order of features
 with open('features.pkl', 'wb') as f:
     pickle.dump(selected_features, f)
 
-# Save encoders
 with open('encoders.pkl', 'wb') as f:
     pickle.dump(encoders, f)
 
-# Train-test split I.E 80/20 TRAINING/TEST SET
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Train model I.E RANDOM FOREST CLASSIFICAtion
+
 model = RandomForestClassifier(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 
-# Save model
+
 with open('fraud_model.pkl', 'wb') as f:
     pickle.dump(model, f)
 
-
+# 9. Print accuracy
 print(f"Model trained. Accuracy: {model.score(X_test, y_test):.2f}")
